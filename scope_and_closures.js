643e7c3594ce366a8e3ef5fb3865db7378e5856a
Anna Khokhrina <valdes.anya@mail.ru>
2025-02-10 12:55:09 +0300

    Modified file: scope_and_closures.js
    Добавлен генератор случайных чисел из диапазона -
    функция createRandomGenerator(min, max),
    которая возвращает новую функцию.
    Возвращенная функция генерирует случайное число в
    пределах от min до max.

--- scope_and_closures.js
+++ scope_and_closures.js
@@ -21,6 +21,25 @@ logger.log("Какое-то сообщение.");
 logger.log("Ещё одно сообщение.");
 logger.log("Тоже сообщение.");
 
+console.log("");
 console.log("Лог сообщений:");
 logger.getLogs();
+console.log("");
+
+
+
+function createRandomGenerator(min, max) {
+    return function() {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
+
+const min = 1; 
+const max = 50; 
+const randomizer = createRandomGenerator(min, max);
+
+console.log("Рандомное число:");
+console.log(randomizer());
+console.log("");
+
 
